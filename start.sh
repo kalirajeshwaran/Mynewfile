6afa66e230b7620f81b05846051793a38530a6bb
kalirajeshwaran <69472407+kalirajeshwaran@users.noreply.github.com>
2022-10-26 18:04:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
 fi
-cd /Eva
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
